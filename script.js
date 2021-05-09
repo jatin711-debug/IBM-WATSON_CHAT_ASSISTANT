c4b4f0cb159c3245f840bb6578bd9368beade943
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-05-08 22:30:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.watsonAssistantChatOptions = 
+        {
+
+            integrationID: "f4928c5a-5b9e-4c58-8717-6111f0ea2257", // The ID of this integration.
+            region: "eu-gb", // The region your integration is hosted in.
+            serviceInstanceID: "abc6cbe6-db99-47fa-a6c0-8fad94138765", // The ID of your service instance.
+            onLoad: (instance) => { instance.render(); 
+            }
+            
+        };
+    setTimeout(()=>{
+    const newScript = document.createElement('script');
+    newScript.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+    document.head.appendChild(newScript);
+});
